ec159fc95c24455be3a91d930c89e156820c5fe3
Pacoautor <fgrautor@gmail.com>
2025-09-27 13:02:17 +0200

    Quitar redirección de favicon del urls.py para evitar 500

--- photopro_app/urls.py
+++ photopro_app/urls.py
@@ -1,29 +1,9 @@
 # photopro_app/urls.py
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic.base import RedirectView
-from django.conf import settings
-from django.contrib.staticfiles.storage import staticfiles_storage
-
-# Redirección de favicon calculada EN CADA REQUEST (no al importar urls)
-class FaviconRedirect(RedirectView):
-    permanent = True
-
-    def get_redirect_url(self, *args, **kwargs):
-        try:
-            # Usa el storage (manifest) en tiempo de petición
-            return staticfiles_storage.url("favicon.ico")
-        except Exception:
-            # Fallback simple por si aún no existe en el manifest
-            return settings.STATIC_URL + "favicon.ico"
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("products.urls")),
-    # si tienes login/logout aquí, déjalos como los tenías
-    # path("login/", ... , name="login"),
-    # path("logout/", ... , name="logout"),
-
-    # Favicon seguro:
-    re_path(r"^favicon\.ico$", FaviconRedirect.as_view()),
 ]
+
