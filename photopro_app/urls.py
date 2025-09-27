b798da878bf20a246b33539b312909554c523ae7
Pacoautor <fgrautor@gmail.com>
2025-09-27 10:47:25 +0200

    Fix favicon route using staticfiles_storage

--- photopro_app/urls.py
+++ photopro_app/urls.py
@@ -1,22 +1,17 @@
-
-
+# photopro_app/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from django.contrib.auth import logout
-from django.shortcuts import redirect
 from django.views.generic.base import RedirectView
-from django.templatetags.static import static
-
-def logout_view(request):
-    logout(request)
-    return redirect("login")
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # 👇 ESTA ES LA LÍNEA NUEVA
-    path("favicon.ico", RedirectView.as_view(url=static("favicon.ico"), permanent=True)),
     path("", include("products.urls")),
-    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path("logout/", logout_view, name="logout"),
+
+    # Favicon correcto (redirecciona a /static/... gestionado por WhiteNoise)
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"), permanent=True),
+        name="favicon"
+    ),
 ]
