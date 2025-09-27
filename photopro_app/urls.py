5a52979b34e50d1c5025f003804fa36441268112
Pacoautor <fgrautor@gmail.com>
2025-09-27 10:38:57 +0200

    Add favicon and map /favicon.ico

--- photopro_app/urls.py
+++ photopro_app/urls.py
@@ -1,17 +1,22 @@
-# photopro_app/urls.py
+
+
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.views.static import serve
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.views.generic.base import RedirectView
+from django.templatetags.static import static
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # 👇 ESTA ES LA LÍNEA NUEVA
+    path("favicon.ico", RedirectView.as_view(url=static("favicon.ico"), permanent=True)),
     path("", include("products.urls")),
-    path("login/", include("django.contrib.auth.urls")),  # opcional si usas las vistas auth
-]
-
-# ⚠️ Servir MEDIA también en producción (Render)
-# No lo limites a DEBUG; en Render lo necesitamos siempre.
-urlpatterns += [
-    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
