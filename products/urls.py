31476edaab1202980735d5e867cc7a362b7c7db5
Pacoautor <fgrautor@gmail.com>
2025-10-25 17:51:58 +0200

    Actualizo runtime.txt y ajustes en urls

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,11 @@
-# products/urls.py
 from django.urls import path
 from . import views
 
-app_name = 'products'
+app_name = "products"
 
 urlpatterns = [
     path('', views.select_category, name='select_category'),
     path('upload/', views.upload_photo, name='upload_photo'),
+    path('processing/', views.processing_view, name='processing'),
     path('result/', views.result_view, name='result'),
 ]
