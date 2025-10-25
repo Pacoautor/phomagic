59a278cad7ed4e5cc5e8ea459057a2b50ad07d27
Pacoautor <fgrautor@gmail.com>
2025-10-25 19:58:31 +0200

    Build sin Django; migrate+collectstatic en start; Python 3.12.6

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Upgrade pip and install dependencies
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-
-# Force DEBUG=true only during build so DB points to BASE_DIR and not /data
-export DEBUG=true
-
-# Only collect static at build time (no DB required, but safe if any code touches DB)
-python manage.py collectstatic --noinput
