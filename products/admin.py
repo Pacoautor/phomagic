96b67b8e6e212cca56b694615ce8c30aa5bf500b
Pacoautor <fgrautor@gmail.com>
2025-10-28 17:34:51 +0100

    Corrige doble registro del modelo Category en admin

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from .models import Category, SubCategory, ViewOption
-
+from .models import Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    search_fields = ('name',)
+    list_display = ('name',)
 
 
 @admin.register(SubCategory)
