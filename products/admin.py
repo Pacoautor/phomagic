ef4571fdba4345e18d66b92e476deac14147179c
Pacoautor <fgrautor@gmail.com>
2025-10-28 17:39:13 +0100

    Limpia y corrige admin.py eliminando duplicados

--- products/admin.py
+++ products/admin.py
@@ -1,30 +1,12 @@
 from django.contrib import admin
-from .models import Category
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-
-
-@admin.register(SubCategory)
-class SubCategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'category')
-    list_filter = ('category',)
-    search_fields = ('name',)
-
-
-@admin.register(ViewOption)
-class ViewOptionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'subcategory')
-    list_filter = ('subcategory',)
-    search_fields = ('name',)
-
 from .models import Category, SubCategory, ViewOption, GeneratedImage
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
     search_fields = ("name",)
+
 
 @admin.register(SubCategory)
 class SubCategoryAdmin(admin.ModelAdmin):
@@ -32,11 +14,13 @@ class SubCategoryAdmin(admin.ModelAdmin):
     list_filter = ("category",)
     search_fields = ("name", "category__name")
 
+
 @admin.register(ViewOption)
 class ViewOptionAdmin(admin.ModelAdmin):
     list_display = ("id", "subcategory", "name")
     list_filter = ("subcategory__category", "subcategory")
     search_fields = ("name", "subcategory__name", "subcategory__category__name")
+
 
 @admin.register(GeneratedImage)
 class GeneratedImageAdmin(admin.ModelAdmin):
