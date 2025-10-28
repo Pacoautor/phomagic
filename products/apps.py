f1cd0649b8994223f10a029aa6dda76a841d5a8f
Pacoautor <fgrautor@gmail.com>
2025-10-28 17:14:25 +0100

    Arreglado conflicto de merge en apps.py

--- products/apps.py
+++ products/apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
 
-
 class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
-<<<<<<< HEAD
-    verbose_name = 'Gestor de Productos e Imágenes'
-=======
->>>>>>> 54dc87cf5bddeb97076c30df6ac7fe69845bb4d6
