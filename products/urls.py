b9945fd9cfd1d957e04de7fddf372cc751659ee1
Pacoautor <fgrautor@gmail.com>
2025-10-29 17:17:56 +0100

    Add GeneratedImage model and views

--- products/urls.py
+++ products/urls.py
@@ -2,18 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Ruta principal - selección de categoría
     path('', views.select_category, name='home'),
-    
-    # AJAX endpoints para cargar opciones dinámicamente
-    path('load_subcategories/', views.load_subcategories, name='load_subcategories'),
-    path('load_views/', views.load_views, name='load_views'),
-    
-    # Flujo principal de la aplicación
     path('upload/', views.upload_photo, name='upload_photo'),
     path('processing/', views.processing, name='processing'),
     path('result/', views.result, name='result'),
-    
-    # Subida de líneas (protegida)
     path('upload-lineas/', views.upload_lineas_zip, name='upload_lineas_zip'),
 ]
--- urls.py
+++ urls.py
@@ -1,14 +1,13 @@
+
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls')),  # 👈 Incluye todas las rutas de la app products
+    path('', include('products.urls')),
 ]
-
-# Servir archivos estáticos y media si estás en modo DEBUG
-from django.conf import settings
-from django.conf.urls.static import static
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
