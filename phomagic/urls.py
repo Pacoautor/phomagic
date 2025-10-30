befe946b837cbe6db9c272018ab90eaa5140c44f
Pacoautor <fgrautor@gmail.com>
2025-10-30 07:44:20 +0100

    Fix: copy correct urls.py to phomagic folder

--- phomagic/urls.py
+++ phomagic/urls.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from products import views  # 👈 Importamos la vista index
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='home'),  # 👈 Esta línea crea la ruta "home"
-    path('products/', include('products.urls')),  # 👈 Incluye las rutas de la app
-]
-
-# Para servir archivos estáticos y media en modo DEBUG
 from django.conf import settings
 from django.conf.urls.static import static
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('products.urls')),
+]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# Force rebuild 
