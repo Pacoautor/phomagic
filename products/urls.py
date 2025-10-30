88484c23f9bfcbcc1f6b5ff286b885db2b6499d2
Pacoautor <fgrautor@gmail.com>
2025-10-30 07:59:48 +0100

    Add select_category route

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.select_category, name='home'),
+    path('select/', views.select_category, name='select_category'),
     path('upload/', views.upload_photo, name='upload_photo'),
     path('processing/', views.processing, name='processing'),
     path('result/', views.result, name='result'),
