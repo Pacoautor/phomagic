978bbc7a91281b29a653db2bf1cfa3037ac32855
Pacoautor <fgrautor@gmail.com>
2025-10-31 07:46:12 +0100

    Rename Lineas to lineas (lowercase) and update files

--- products/utils.py
+++ products/utils.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),  # 👈 Ruta raíz dentro de products
-    path('generate/', views.generate_image, name='generate_image'),
+    path('', views.select_category, name='home'),
+    path('select/', views.select_category, name='select_category'),
+    path('upload/', views.upload_photo, name='upload_photo'),
     path('processing/', views.processing, name='processing'),
     path('result/', views.result, name='result'),
+    path('upload-lineas/', views.upload_lineas_zip, name='upload_lineas_zip'),
 ]
