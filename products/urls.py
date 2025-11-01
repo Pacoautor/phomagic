2222b8bb38a4bb3f2af385609f685061d19d0076
Pacoautor <fgrautor@gmail.com>
2025-11-01 08:50:45 +0100

    Fix: eliminado select_category inexistente en urls.py

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.select_category, name='home'),
+    path('', views.index, name='home'),
+
     path('select/', views.select_category, name='select_category'),
     path('upload/', views.upload_photo, name='upload_photo'),
     path('processing/', views.processing, name='processing'),
