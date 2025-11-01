f22d1de83419d9de4a85474ffe1d5097123dad91
Pacoautor <fgrautor@gmail.com>
2025-11-01 08:57:26 +0100

    Fix: urls limpias y coherentes con vistas actuales

--- products/urls.py
+++ products/urls.py
@@ -2,11 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    # Página principal: subir imagen
+    path('', views.upload_photo, name='home'),
 
-    path('select/', views.select_category, name='select_category'),
+    # Flujo de vistas
     path('upload/', views.upload_photo, name='upload_photo'),
     path('processing/', views.processing, name='processing'),
-    path('result/', views.result, name='result'),
-    path('upload-lineas/', views.upload_lineas_zip, name='upload_lineas_zip'),
+
+    # Opciones y selección
+    path('view-options/', views.view_options, name='view_options'),
+    path('select-category/', views.select_category_view, name='select_category_view'),
+
+    # Resultados
+    path('result/', views.result_view, name='result_view'),
 ]
