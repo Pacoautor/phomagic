2f7b0699d62cfdcce7fd7646907de8db9ff37a5c
Pacoautor <fgrautor@gmail.com>
2025-11-05 13:17:13 +0100

    Fix media serving

--- photopro_app/urls.py
+++ photopro_app/urls.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
 ]
 
-# Servir archivos media en desarrollo y producción
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    # En producción también necesitamos servir los archivos media
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Servir archivos media SIEMPRE (desarrollo y producción)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
